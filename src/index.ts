51bd87cb70ec87f27bf2bde7f676d89cfbabad34
Devinda Senanayaka <3579142+dca123@users.noreply.github.com>
2023-07-12 15:02:32 +0800

    f

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,23 @@ import { Workbook } from "exceljs";
 import { createReadStream, readFileSync } from "fs";
 
 async function main() {
+  console.time("main");
+  console.time("createWorkbook");
   const workbook = new Workbook();
+  console.timeEnd("createWorkbook");
+
+  console.time("readFile");
   const csv = createReadStream("./src/input/coffee.csv");
+  console.timeEnd("readFile");
+
+  console.time("attachSheet");
   await workbook.csv.read(csv);
+  console.timeEnd("attachSheet");
+
+  console.time("writeFile");
   await workbook.xlsx.writeFile("./src/output/coffee.xlsx");
+  console.timeEnd("writeFile");
+
+  console.timeEnd("main");
 }
-console.time("main");
 main();
-console.timeEnd("main");
